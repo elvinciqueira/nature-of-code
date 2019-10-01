96bb930f265b0a347e0491f325b50f26f6d375b5
Elvin Ciqueira <elvinciqueira@gmail.com>
2019-10-01 18:59:42 -0300

    object accelerates towards the mouse

---
+++ chp01_vectors/NOC_1_10_motion101_acceleration/sketch.js
@@ -0,0 +1,17 @@
+//The Nature of Code
+//Elvin Ciqueira
+//natureofcode.com
+
+let mover;
+
+function setup() {
+  createCanvas(640, 360);
+  mover = new Mover();
+}
+
+function draw() {
+  background(51);
+
+  mover.update();
+  mover.display();
+}
