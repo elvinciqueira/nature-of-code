11b9675972695a1495c8455be3639d83266aba0d
Elvin Ciqueira <elvinciqueira@gmail.com>
2019-10-21 22:15:18 -0300

    Pointing in the direction of motion

---
+++ chp03_oscillation/NOC_3_03_pointing_velocity/sketch.js
@@ -0,0 +1,18 @@
+//The Nature of Code
+//Elvin Ciqueira
+//natureofcode.com
+
+let mover;
+
+function setup() {
+  createCanvas(640, 360);
+  mover = new Mover();
+}
+
+function draw() {
+  background(50);
+
+  mover.display();
+  mover.update();
+  mover.checkEdges();
+}
