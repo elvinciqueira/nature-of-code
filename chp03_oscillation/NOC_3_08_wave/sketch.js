18b4754d9d06b3e6ba08d770317f748e6e0ffcea
Elvin Ciqueira <elvinciqueira@gmail.com>
2019-10-30 14:20:01 -0300

    sin wave

---
+++ chp03_oscillation/NOC_3_08_wave/sketch.js
@@ -0,0 +1,26 @@
+//The Nature of Code
+//Elvin Ciqueira
+//natureofcode.com
+
+let startAngle = 0;
+let angleVel = 0.23;
+
+function setup() {
+  createCanvas(640, 360);
+}
+
+function draw() {
+  background(51);
+
+  startAngle += 0.015;
+  let angle = startAngle;
+
+  for (let x = 0; x <= width; x += 24) {
+    let y = map(sin(angle), -1, 1, 0, height);
+    stroke(164);
+    fill(255, 50);
+    strokeWeight(2);
+    ellipse(x, y, 48, 48);
+    angle += angleVel;
+  }
+}
